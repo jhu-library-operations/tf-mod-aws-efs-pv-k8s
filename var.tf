5dd4af45d9175eb4b262043292b85611a228565e
Derek Belrose <derek@derekbelrose.com>
2020-10-20 15:17:12 -0400

    Initially commit

---
+++ var.tf
@@ -0,0 +1,39 @@
+variable "project_name" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "tags" {
+  default = {}
+}
+variable "output_path" {}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "vpc_cidr_block" {
+  type = string
+}
+
+variable "volume_label" {
+  type = string
+}
+
+variable "volume_capacity" {
+  type = string
+  default = "5Gi"
+}
+
+variable "volume_access_mode" {
+  type = string
+  default = "ReadWriteMany"
+}
+
+variable "volume_reclaim_policy" {
+  type = string
+  default = "Retain"
+}
